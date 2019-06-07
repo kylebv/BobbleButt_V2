3c95af18613cc712f6829fbe7fa1a64513a57535
Robert Piercy <c3282915@uon.edu.au>
2019-06-07 22:22:24 +1000

    Final Change

--- BobbleButt/PresentationLayer/PageHeader.Master.cs
+++ BobbleButt/PresentationLayer/PageHeader.Master.cs
@@ -8,9 +8,8 @@ using System.Web.UI.WebControls;
 
 namespace BobbleButt
 {
-    public partial class PageHeader : basePageSessionExpire
+    public partial class PageHeader : System.Web.UI.MasterPage
     {
-        //System.Web.UI.MasterPage :
         protected List<Product> cart;
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -18,61 +17,6 @@ namespace BobbleButt
             //Session.Add("cart", GlobalData.productList);
             cart = (List<Product>)Session["cart"];
 
-            //Session["user_id"] = 1;
-            //Session.Add("user", GlobalData.userMap["adminuser@bobblebutt.com"]);
-            //if (HttpContext.Current.Session == null)
-            //{
-            //Response.Redirect("Main?sessionTimeout=true");
-            //}
-            //if (Session[""]==null)
-            //{
-            //Session.Add("new session pls", "hooray");
-            //Response.Redirect("Main?sessionTimeout=true");
-            //}
-
-            //if (Session.Contents.Count == 0)
-            //{
-            //Session.Add("new session pls", "hooray");
-            //Response.Redirect("Main?sessionTimeout=true");           
-            //}
-
-            /*if (HttpContext.Current.Session != null)
-                {
-                    if (HttpContext.Current.Session.IsNewSession)
-                    {
-                        string CookieHeaders = HttpContext.Current.Request.Headers["Cookie"];
-
-                        if ((null != CookieHeaders) && (CookieHeaders.IndexOf("ASP.NET_SessionId") >= 0))
-                        {
-                            // IsNewSession is true, but session cookie exists,
-                            // so, ASP.NET session is expired
-                            Response.Redirect("Main?sessionTimeout=true");
-                        }
-                    }
-                }*/
-
-            //if (Session.IsNewSession)
-            //{
-            //  Response.Redirect("Main?sessionTimeout=true");
-            //}
-
-            /*if (Context.Session != null)
-            {
-                if (Session.IsNewSession)
-                {
-                    string szCookieHeader = Request.Headers["Cookie"];
-                    if ((null != szCookieHeader) && (szCookieHeader.IndexOf("ASP.NET_SessionId") >= 0))
-                    {
-                        Response.Redirect("Main?sessionTimeout=true");
-                    }
-                }
-            }*/
-            //if (Convert.ToString(Session["EmpID"]) == "")
-          
-              //  {
-                //    Session.Add("new session pls", "hooray");
-                  //  Response.Redirect("Main?sessionTimeout=true");
-                //}
         }
 
         protected void btnSearch_Click(object sender, EventArgs e)
